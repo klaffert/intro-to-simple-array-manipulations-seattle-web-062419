9fd6a61f72b8b4d8af8e85db2a074aa48967c558
Kelliann Lafferty <kell10192@gmail.com>
2019-05-31 22:52:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,3 +4,7 @@ end
 # => using_push takes in two arguments, an array and a string, and adds that string to the end of the array using the push method 
 # => "Benin", "Carpe Verde", "Gambia", "Liberia", "Mali", "Niger"
 
+def using_unshift(array,string)
+  array.unshift(string)
+end 
+#> using_unshift takes in two arguments, an array and a string, and adds that string to the front of the array using the unshift method
