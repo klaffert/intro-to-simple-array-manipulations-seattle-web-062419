3a7c8ae4490beeb7eb1b7d46a9d62ce26ecf4452
Kelliann Lafferty <kell10192@gmail.com>
2019-05-31 22:50:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,5 @@ def using_push(array,string)
   array.push(string)
 end
 # => using_push takes in two arguments, an array and a string, and adds that string to the end of the array using the push method 
-# = > 
+# => "Benin", "Carpe Verde", "Gambia", "Liberia", "Mali", "Niger"
+
