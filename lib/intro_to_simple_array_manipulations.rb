026710c312e8061b4df79179ce39193031954862
Kelliann Lafferty <kell10192@gmail.com>
2019-05-31 22:59:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,4 +20,10 @@ def pop_with_args(array)
   array.pop(2)
 end 
 # => takes in an argument of an array and uses the pop method with an argument of 2 to remove the last two array items and returns them; decreases the length of the array by 2 
-# =>
+# => "Stable Boy", "Sandor Clegane"
+
+def using_shift(array)
+array.shift
+end
+# => takes in an argument of an array and uses the shift method to remove the first item and return item
+# => 
