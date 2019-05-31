e5368e75e099f8aa1946a53363329d1057d417ac
Kelliann Lafferty <kell10192@gmail.com>
2019-05-31 22:52:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,4 +8,4 @@ def using_unshift(array,string)
   array.unshift(string)
 end 
 # => using_unshift takes in two arguments, an array and a string, and adds that string to the front of the array using the unshift method
-# => 
+# => "Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"
